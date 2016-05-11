dead525012916b51f9d8a7694b0723fd48e5a2d9
Mikkel S <mikkel0_0@live.dk>
2016-05-11 14:10:01 +0200

    Files

---
+++ Assets/Actions.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Actions : MonoBehaviour {
+
+    public Transform target;
+    public int damp = 5;
+	// Use this for initialization
+	void Start () {
+	
+	}
+     
+ void Update()
+    {
+        if (target) // we get sure the target is here
+        {
+            var rotationAngle = Quaternion.LookRotation(target.position - transform.position); // we get the angle has to be rotated
+            Debug.Log(rotationAngle);
+            Debug.Log(target);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotationAngle, Time.deltaTime * damp); // we rotate the rotationAngle 
+        }
+    }
+}
---
+++ Assets/movement.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movement : MonoBehaviour {
+
+    Rigidbody2D Rigidbody;
+    float moveSpeed = 10;
+    float hMove;
+    float vMove;
+    Vector2 vel;
+    Vector2 mousePosition;
+    float speed = 2f;
+    float horizontalSpeed = 2f;
+    float verticalSpeed = 2f;
+	// Use this for initialization
+	void Start () {
+        Rigidbody = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        hMove = Input.GetAxis("Horizontal");
+        vMove = Input.GetAxis("Vertical");
+
+        vel = new Vector2(hMove, vMove);
+
+        Rigidbody.velocity = vel * moveSpeed;
+        
+    }
+}
